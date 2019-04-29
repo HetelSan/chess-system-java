89e87f5671bddeb823fe78301a5e1caedd95b21b
Hetel Santos <hetel.santos@gmail.com>
2019-04-29 16:05:48 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		// converte a posição to tabuleiro para posição na matriz.
 		Position source = sourcePosition.toPosition();
